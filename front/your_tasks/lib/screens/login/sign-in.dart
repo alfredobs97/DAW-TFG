f8030087226b6c4263e58f1fb309fa2f75a14a16
alfredo-codigobase <alfredo@codigobase.es>
2020-06-07 13:01:30 +0200

    remove ;

--- front/your_tasks/lib/screens/login/sign-in.dart
+++ front/your_tasks/lib/screens/login/sign-in.dart
@@ -31,7 +31,6 @@ class _SignInState extends State<SignIn> {
             context.bloc<TaskBloc>().add(FetchTask(state.username));
             Navigator.pushNamed(context, '/home');
           }
-          ;
         },
         child: Column(
           children: <Widget>[
